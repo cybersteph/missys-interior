444fcc28e15111f028d4f536b869415e38b74afd
cybersteph <thecybersteph@gmail.com>
2025-11-18 16:41:14 -0700

    chore: update git page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/main.jsx',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
     devServer: {
