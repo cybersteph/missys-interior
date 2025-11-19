b1ad6aaee817ba291cffc2b3d0fc1e1b919100e1
cybersteph <thecybersteph@gmail.com>
2025-11-18 19:34:56 -0700

    fix: header image in mobile

--- src/Sections/Header/Header.jsx
+++ src/Sections/Header/Header.jsx
@@ -3,22 +3,22 @@ import "./Header.css"; // optional stylesheet
 
 const Header = () => {
   return (
-      <section className="header">
-        <div className="header-content">
-          <h1>Creating experiences by transforming any space.</h1>
-          <h2>Moments made beautiful.</h2>
-          <p>I don’t just decorate spaces — I craft environments that tell stories. 
-            From curated home and office designs to beautifully themed events, parties, 
-            and special celebrations, I transform any space into a moment you’ll never forget.</p>
-          <a
-            href="mailto:missysignaturespaces@gmail.com?subject=%5BType%5D%20Consultation%20Request"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="header-button">Let's Get Started</button>
-          </a>
-        </div>
-      </section>
+    <section className="header">
+      <div className="header-content">
+        <h1>Creating experiences by transforming any space.</h1>
+        <h2>Moments made beautiful.</h2>
+        <p>I don’t just decorate spaces — I craft environments that tell stories. 
+          From curated home and office designs to beautifully themed events, parties, 
+          and special celebrations, I transform any space into a moment you’ll never forget.</p>
+        <a
+          href="mailto:missysignaturespaces@gmail.com?subject=%5BType%5D%20Consultation%20Request"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="header-button">Let's Get Started</button>
+        </a>
+      </div>
+    </section>
   );
 };
 
